2482749b4bfc1fe4784d68aee359a86b9d46411b
sam <lad.sangram@gmail.com>
2020-08-24 16:19:22 +0100

    Initial commit

---
+++ testApp/Data/Word.cs
@@ -0,0 +1,8 @@
+﻿namespace testApp.Data
+{
+    public class Word
+    {
+        public string  charchter { get; set; }
+        public int repeat { get; set; }
+    }
+}
---
+++ testApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using testApp.Data;
+
+namespace testApp
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration;
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            
+            using (var input = new StreamReader(Configuration.GetSection("filePath").Value))
+            {
+                var wordList = input.ReadToEnd().Split(null);
+                var output = wordList.GroupBy(x => x)
+                    .Select(x => new Word {charchter = x.Key, repeat = x.Count()})
+                    .OrderByDescending(x => x.repeat);
+                foreach (var item in output)
+                {
+                    Console.WriteLine($"For word '{item.charchter}' count is :{item.repeat}");
+                }
+            }
+        }
+    }
+}
